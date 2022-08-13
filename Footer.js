0ac8eb98bbcfc82a57f08d0a100ebae4e6392517
Hameedarh <107671824+Hameedarh@users.noreply.github.com>
2022-08-13 22:14:28 +0100

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div>
+      <div>
+        <h4>© Copyright 2022 | Team-One</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
